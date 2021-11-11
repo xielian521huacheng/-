c29187aac26100b1f359efd802bccf5b22a4668e
xielian521huacheng <740559577@qq.com>
2021-11-11 09:44:45 +0800

    导航栏头部

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,7 +1,56 @@
 <template>
-	<view>
-		
-	</view>
+  <div class="cart-box">
+    <s-header :title="'购物车'"></s-header>
+    <div class="cart-body">
+      <van-checkbox-group @change="groupChange" v-model="result" ref="checkboxGroup">
+        <van-swipe-cell :right-width="50" v-for="(item, index) in list" :key="index">
+          <div class="good-item">
+            <van-checkbox :name="item.cartItemId" />
+            <div class="good-img"><img :src="`//lmall.xinfeng.site${item.goodsCoverImg}`" alt=""></div>
+            <div class="good-desc">
+              <div class="good-title">
+                <span>{{ item.goodsName }}</span>
+                <span>x{{ item.goodsCount }}</span>
+              </div>
+              <div class="good-btn">
+                <div class="price">¥{{ item.sellingPrice }}</div>
+                <van-stepper
+                  integer
+                  :min="1"
+                  :value="item.goodsCount"
+                  :name="item.cartItemId"
+                  async-change
+                  @change="onChange"
+                />
+              </div>
+            </div>
+          </div>
+          <van-button
+            slot="right"
+            square
+            icon="delete"
+            type="danger"
+            class="delete-button"
+            @click="deleteGood(item.cartItemId)"
+          />
+        </van-swipe-cell>
+      </van-checkbox-group>
+    </div>
+    <van-submit-bar
+      v-if="list.length > 0"
+      class="submit-all"
+      :price="total * 100"
+      button-text="结算"
+      @submit="onSubmit"
+    >
+      <van-checkbox @click="allCheck" v-model="checkAll">全选</van-checkbox>
+    </van-submit-bar>
+    <div class="empty" v-if="!list.length">
+      <van-icon name="smile-o" />
+      <div class="title">购物车空空空如也</div>
+      <van-button color="#c40000" type="primary" @click="goTo" block>前往首页</van-button>
+    </div>
+  </div>
 </template>
 
 <script>
